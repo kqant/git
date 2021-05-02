dc557ee647a74382f8d9686fa8105c97b94eff7d
kqant <32996046+kqant@users.noreply.github.com>
2021-05-02 17:57:23 +0500

    Add files via upload

---
+++ graf2.py
@@ -0,0 +1,28 @@
+def comp_sv():
+
+    def dfs(v):
+        used.append(v)
+        for u in g[v]:
+            if u not in used:
+                dfs(u)
+
+    n = 0
+    for v in g:
+        if v not in used:
+            dfs(v)
+            n += 1
+    print(n)
+
+
+V = int(input())  # Кол-во вершин
+used = [False] * V
+g = []
+gtemp = {i: set() for i in range(V)}
+for i in range(V):
+    g.append(list(map(int, input().split())))
+for i in range(V):
+    for j in range(V):
+        if g[i][j]:
+            gtemp[i].add(j)
+
+print("Есть циклы" if flag else "Циклов нет")
